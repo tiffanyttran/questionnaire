c6feb5a3627f5e9d99a651afa0fb28e54a3e0698
tiffanyttran <93799662+tiffanyttran@users.noreply.github.com>
2023-04-26 23:01:48 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import AWS from 'aws-sdk';
 
-function App() {
+const QuestionnaireResponses = () => {
+  const [responses, setResponses] = useState([]);
+
+  useEffect(() => {
+    // Initialize AWS SDK
+    AWS.config.update({
+      accessKeyId: 'YOUR_ACCESS_KEY',
+      secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
+      region: 'YOUR_AWS_REGION',
+    });
+
+    // Create DynamoDB instance
+    const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+    // Fetch questionnaire responses from DynamoDB
+    const fetchResponses = async () => {
+      try {
+        const params = {
+          TableName: 'QuestionnaireResponses',
+        };
+
+        const data = await dynamoDB.scan(params).promise();
+        setResponses(data.Items);
+      } catch (error) {
+        console.error('Error fetching questionnaire responses:', error);
+      }
+    };
+
+    fetchResponses();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Questionnaire Responses</h1>
+      {responses.map((response) => (
+        <div key={response.responseId}>
+          <h3>{response.question}</h3>
+          <p>{response.answer}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default App;
+export default QuestionnaireResponses;
